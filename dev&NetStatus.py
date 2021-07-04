5b0ff6279fdc1e95b69bdecff84906caaf1a3c79
E-L <45451697+ekow-lloyd@users.noreply.github.com>
2021-07-04 21:23:10 +0200

    Update dev&NetStatus.py

--- dev&NetStatus.py
+++ dev&NetStatus.py
@@ -4,31 +4,31 @@ import smtplib
 import subprocess 
 
 while True:
-    result = subprocess.run(['ping', '-c', '3','-n', '192.168.0.136'],stdout=subprocess.PIPE)
+    result = subprocess.run(['ping', '-c', '3','-n', '192.168.0.136'],stdout=subprocess.PIPE). # the ping command and IP address of my camera
 
     outcome = result.returncode
 
-    if outcome == 0:
+    if outcome == 0:  # a return code of '0' means succesful ping, then this block of code sends a success report
         smtpObj = smtplib.SMTP('smtp.gmail.com', 587)
         test= smtpObj.ehlo()      # establish a test connection, response 250 means succesful connection.
         encrypt = smtpObj.starttls()
-        signIn = smtpObj.login('ekowlloyd@gmail.com','M@rley-Afeni2020')
-        sendMsg = smtpObj.sendmail("ekowlloyd@gmail", "lloyd.stewart@alpro.com", "Subject: Status : Online \nYo stewart, Your device is still online.\nKind Regards")
+        signIn = smtpObj.login('myemail@gmail.com','Pa55wordWith5')
+        sendMsg = smtpObj.sendmail("myemail@gmail", "recipientemail@gmail.com", "Subject: Status : Online \nYo stewart, Your device is still online.\nKind Regards")
         {}
         signOut = smtpObj.quit()
 
         print("\n Success!! \n")
-
+        
         print(signOut)
         print()
         quit()
         
-    else:
+    else:  # if the return code was anything other than '0', means a failure and device is down, so an alert email is sent
         smtpObj = smtplib.SMTP('smtp.gmail.com', 587)
         test = smtpObj.ehlo()
         encrypt = smtpObj.starttls()
         signIn = smtpObj.login('myemail@gmail.com','Pa55wordwith"5"') # credentials to log in to email-account
-        sendMsg = smtpObj.sendmail("myemail@gmail", "receipeintemail@gmail.com", "Subject: Alert!! \nYo stewart, Device is Offline.\nKind Regards")
+        sendMsg = smtpObj.sendmail("myemail@gmail", "recipientemail@gmail.com", "Subject: Alert!! \nYo stewart, Device is Offline.\nKind Regards")
         {}
         signOut = smtpObj.quit()
 
