c86702a4a5fcdcdb400b99e224fd399d063c9da9
eKow <45451697+ekow-lloyd@users.noreply.github.com>
2021-07-09 00:41:09 +0200

    Update dev&NetStatus.py
    added additional code to enable the program to identify which devices are online and which aren't.

--- dev&NetStatus.py
+++ dev&NetStatus.py
@@ -3,37 +3,52 @@
 import smtplib
 import subprocess 
 
-while True:
-    result = subprocess.run(['ping', '-c', '3','-n', '192.168.0.136'],stdout=subprocess.PIPE). # the ping command and IP address of my camera
+Online = []
+Offline = []
 
-    outcome = result.returncode
+def sendMail():
+    smtpObj = smtplib.SMTP('smtp.gmail.com', 587)
+    smtpObj.ehlo()
+    smtpObj.starttls()
+    smtpObj.login("pythonblac@gmail.com","Dede@feni1520")
+    smtpObj.sendmail("pythonblack@gmail.com", "lloyd.stewart@alpro.com", "Subject:Device State \nHi Lloyd,\nthe following device(s) are Online at time of mail: {}.\nCheers,\nPythonBlac".format(Online))
+    smtpObj.quit()
 
-    if outcome == 0:  # a return code of '0' means succesful ping, then this block of code sends a success report
-        smtpObj = smtplib.SMTP('smtp.gmail.com', 587)
-        test= smtpObj.ehlo()      # establish a test connection, response 250 means succesful connection.
-        encrypt = smtpObj.starttls()
-        signIn = smtpObj.login('myemail@gmail.com','Pa55wordWith5')
-        sendMsg = smtpObj.sendmail("myemail@gmail", "recipientemail@gmail.com", "Subject: Status : Online \nHi there, Your device is online and running.\nKind Regards")
-        {}
-        signOut = smtpObj.quit()
+sendMail()
 
-        print("\n Success!! \n")
-        
-        print(signOut)
-        print()
-        quit()
-        
-    else:  # if the return code was anything other than '0', means a failure and device is down, so an alert email is sent
-        smtpObj = smtplib.SMTP('smtp.gmail.com', 587)
-        test = smtpObj.ehlo()
-        encrypt = smtpObj.starttls()
-        signIn = smtpObj.login('myemail@gmail.com','Pa55wordwith"5"') # credentials to log in to email-account
-        sendMsg = smtpObj.sendmail("myemail@gmail", "recipientemail@gmail.com", "Subject: Alert!! \nHi there, Device is Offline.\nKind Regards")
-        {}
-        signOut = smtpObj.quit()
 
-        print('\n Failed!! \n')
-        print(signOut)
+def mailFail():
+    smtpObj = smtplib.SMTP('smtp.gmail.com', 587)
+    smtpObj.ehlo()
+    smtpObj.starttls()
+    smtpObj.login("pythonblac@gmail.com","Dede@feni1520")
+    smtpObj.sendmail("pythonblac@gmail.com","lloyd.stewart@alpro.com","Subject:Alert \nHi there,\nthe following device(s)are offline : {}".format(Offline))
+    smtpObj.quit()
+mailFail()
+
+
+with open('pinglist.txt', 'r') as readfile:
+    for IP in readfile:
+        print('')
+        print(IP.rstrip())
+        print('Pinging:', IP.rstrip())
+        result = subprocess.run(['ping', '-c', '1','-n', IP.strip()],stdout=subprocess.PIPE)
+        outcome = result.returncode
+
+        if outcome == 0:
+            Online.append(IP.rstrip())
+            print("Status: Online")
+            print('Sending confirmation mail....')
+            sendMail()
+        else:
+            if outcome != 0:
+                Offline.append(IP.rstrip())
+                print("Status: Down")
+                print('Sending Notification mail')
+                mailFail()
+
+print('Exiting Status Check')
+print('')
 
 
 
